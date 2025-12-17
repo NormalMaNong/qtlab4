1fb42329db383bda4741f923404b91a29506fa62
NormalMaNong <1512889762@qq.com>
2025-12-17 20:18:02 +0800

    实现基本的槽函数原型及辅助调试功能

--- ChatServer/mainwindow.cpp
+++ ChatServer/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,23 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_startStopButton_clicked()
+{
+    static bool started = false;
+    started = !started;
+    if(started){
+        ui->startStopButton->setText("停止服务器");
+        logMessage("服务器已经启动");
+    }
+    else{
+        ui->startStopButton->setText("启动服务器");
+        logMessage("服务器已经停止");
+    }
+}
+
+void MainWindow::logMessage(const QString &msg)
+{
+    ui->logEditor->appendPlainText(msg);
+}
+
